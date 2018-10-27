7d73954de2a665fc38c04d3142b69ae9d83ee058
50U10FCA7 <31173478+50U10FCA7@users.noreply.github.com>
2018-10-28 00:25:40 +0300

    remove hello world :^)

--- linux/gman.sh
+++ linux/gman.sh
@@ -7,7 +7,6 @@ else
 fi
 
 if [ -z "$2" ]; then
-	echo "hello world"
 	read -p "Enter script name: " script
 else
 	script=$2
@@ -15,14 +14,14 @@ fi
 
 
 
-#config
+#config_repositories_remote - sets without '/' at start and end
+#config_repositories_local - sets without '/' at start and end, locals from gmancl directory
 config_user="username"
 config_ip='127.0.0.1'
 config_port="1337"
 config_repositories_remote="home/username/gman_repositories"
 config_repositories_local="repositories"
-#config_repositories_remote - sets without '/' at start and end
-#config_repositories_local - sets without '/' at start and end, locals from gmancl directory
+#/config
 
 
 
